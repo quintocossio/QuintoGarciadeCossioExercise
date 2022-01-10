f31c90cfc78f6d9e124f138421dc012acb990798
Quinto <quintocossio@gmail.com>
2022-01-10 16:56:23 -0300

    Initial Commit

---
+++ AlbumExerciseApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlbumExerciseApp
+//
+//  Created by Quinto Cossio on 10/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
