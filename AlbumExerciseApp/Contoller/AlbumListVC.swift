6aebef43f93fb042ea90cf04232c0abdf5f1408c
Quinto <quintocossio@gmail.com>
2022-01-11 10:44:37 -0300

    Filter and Search Albums

--- AlbumExerciseApp/Contoller/AlbumListVC.swift
+++ AlbumExerciseApp/Contoller/AlbumListVC.swift
@@ -12,10 +12,22 @@ class AlbumListVC: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     var albums = [Album]()
+    var filteredAlbums = [Album]()
+    let searchController = UISearchController(searchResultsController: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Buscar Album..."
+        searchController.hidesNavigationBarDuringPresentation = false
+        navigationItem.searchController = searchController
+        definesPresentationContext = false
 
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.estimatedRowHeight = 80
+        
         loadAlbums()
 
     }
@@ -47,8 +59,31 @@ class AlbumListVC: UIViewController {
                 }
             }
         }
+    
+    //MARK: - SEARCH METHODS
+    
+    func searchBarIsEmpty() -> Bool {
+        // Returns true if the text is empty or nil
+        return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        
+        filteredAlbums = albums.filter({ (album: Album) -> Bool in
+            return album.title.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData()
         
     }
+    
+    func isFiltering() -> Bool {
+        let searchBarScopeIsFiltering = searchController.searchBar.selectedScopeButtonIndex != 0
+        return searchController.isActive && (!searchBarIsEmpty() || searchBarScopeIsFiltering)
+    }
+    
+        
+}
     
 
 
@@ -58,19 +93,38 @@ extension AlbumListVC: UITableViewDelegate, UITableViewDataSource{
             return 0
         }
         
+        if isFiltering(){
+            return filteredAlbums.count
+        }
+        
         return albums.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "AlbumCell", for: indexPath) as! AlbumCell
         
-        let album = albums[indexPath.row]
+        let album:Album
+        
+        if isFiltering(){
+            album = filteredAlbums[indexPath.row]
+        }else{
+            album = albums[indexPath.row]
+        }
         
         cell.titleLbl.text = album.title
         
         return cell
     }
     
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
     
     
+}
+
+extension AlbumListVC: UISearchResultsUpdating{
+    func updateSearchResults(for searchController: UISearchController) {
+        filterContentForSearchText(searchController.searchBar.text!)
+    }
 }
